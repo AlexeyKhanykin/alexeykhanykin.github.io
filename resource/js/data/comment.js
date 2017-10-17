aa19d79f9bab1efd32b3f9bd36b2791ba06e8b90
AlexeyKhanykin <alexeykhanykin@gmail.com>
2017-10-17 18:25:30 +0400

    Add files via upload

---
+++ resource/js/data/comment.js
@@ -0,0 +1,7 @@
+var UserComment = function(text) {
+      this.text = text;
+}
+
+$.extend(UserComment.prototype, {
+
+});
---
+++ resource/js/data/post.js
@@ -0,0 +1,9 @@
+var Post = function(name, content, comments) {
+    this.name = name;
+    this.content = content;
+    this.comments = comments;
+}
+
+$.extend(Post.prototype, {
+
+});
